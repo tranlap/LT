cbb0fb767c935b7e91ff8557f1382ea8e380f5c6
tranlap <tranlap1231@gmail.com>
2017-04-23 14:42:08 +0700

    commit

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ltzr.vn/upload/admin/');
+define('HTTP_CATALOG', 'http://ltzr.vn/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ltzr.vn/upload/admin/');
+define('HTTPS_CATALOG', 'http://ltzr.vn/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/upload/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/upload/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/upload/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/upload/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/upload/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/upload/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/upload/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
